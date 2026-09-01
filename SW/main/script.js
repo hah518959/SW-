a9a6c4f12e0bbdeaf46c6c230674a86b85ea577d
hah518959 <hah518959@gmail.com>
2026-09-01 15:24:15 +0900

    Add files via upload

---
+++ SW/main/script.js
@@ -0,0 +1,10 @@
+let b1, b2, b3, b4, b5, b6, b7;
+
+b1 = document.querySelector("book1");
+b2 = document.querySelector("book2");
+b3 = document.querySelector("book3");
+b4 = document.querySelector("book4");
+b5 = document.querySelector("book5");
+b6 = document.querySelector("book6");
+b7 = document.querySelector("book7");
+
